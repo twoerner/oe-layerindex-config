635de68482a96f74b00cc4eafe7ff8d62c60ef7f
Trevor Woerner <twoerner@gmail.com>
2015-06-14 08:17:59 -0400

    oesetup.sh: second and subsequent runs
    The first time oesetup.sh is run it needs to get the user to perform a full
    configuration. For all subsequent runs it only needs to update the user's
    environment. Add the logic necessary for the subsequent runs.
    
    Signed-off-by: Trevor Woerner <twoerner@gmail.com>

--- oesetup.sh
+++ oesetup.sh
@@ -23,43 +23,52 @@ cleanup() {
 	unset DL_DIR
 }
 
-# find programs
-if [ ! -x $RUNFROMDIR/pysetup-int.py ]; then
-	echo "can't find internal setup routine $RUNDROMDIR/pysetup-int.py"
-	cleanup
-	return 1
-fi
-which dialog > /dev/null 2>&1
-if [ $? -ne 0 ]; then
-	echo "ERROR: can't find 'dialog' command"
-	return 1
-fi
-
-# find local.conf
-if [ ! -r $RUNFROMDIR/local.conf ]; then
-	echo "can't find config file $RUNFROMDIR/local.conf"
-	cleanup
-	return 1
-fi
-
+# setup build dir and check if this is the first time or not
 BUILDDIR=${1:-build}
-if [ "$(whoami)" = "root" ]; then
-	echo "ERROR: please don't run this as root!"
-	cleanup
-	return 1
-fi
+if [ -d "$BUILDDIR" -a -r "$BUILDDIR/conf/local.conf" ]; then
+	echo "It looks like a build has already been configured in $BUILDDIR"
+	echo "Updating environment..."
+else
 
-$RUNFROMDIR/pysetup-int.py $BUILDDIR
-if [ $? -ne 0 ]; then
-	cleanup
-	return 1
-fi
+	# find programs
+	if [ ! -x $RUNFROMDIR/pysetup-int.py ]; then
+		echo "can't find internal setup routine $RUNDROMDIR/pysetup-int.py"
+		cleanup
+		return 1
+	fi
+	which dialog > /dev/null 2>&1
+	if [ $? -ne 0 ]; then
+		echo "ERROR: can't find 'dialog' command"
+		return 1
+	fi
 
-# specify DL_DIR
-read -e -p "Please indicate the location of your DL_DIR (downloads directory): " DL_DIR
-cat >> $BUILDDIR/conf/auto.conf <<-EOF
-	DL_DIR ?= "${DL_DIR}"
-EOF
+	# find local.conf
+	if [ ! -r $RUNFROMDIR/local.conf ]; then
+		echo "can't find config file $RUNFROMDIR/local.conf"
+		cleanup
+		return 1
+	fi
+
+	if [ "$(whoami)" = "root" ]; then
+		echo "ERROR: please don't run this as root!"
+		cleanup
+		return 1
+	fi
+
+	$RUNFROMDIR/pysetup-int.py $BUILDDIR
+	if [ $? -ne 0 ]; then
+		cleanup
+		return 1
+	fi
+
+	# specify DL_DIR
+	read -e -p "Please indicate the location of your DL_DIR (downloads directory): " DL_DIR
+	cat >> $BUILDDIR/conf/auto.conf <<-EOF
+		DL_DIR ?= "${DL_DIR}"
+	EOF
+
+	cp $RUNFROMDIR/local.conf conf
+fi
 
 # Make sure Bitbake doesn't filter out the following variables from our environment.
 export BB_ENV_EXTRAWHITE="MACHINE DISTRO TCMODE TCLIBC HTTP_PROXY http_proxy \
@@ -75,7 +84,6 @@ export PATH=$NEWPATHS$(echo $PATH | sed -e "s|:$NEWPATHS|:|g" -e "s|^$NEWPATHS||
 unset NEWPATHS
 
 cd $BUILDDIR
-cp $RUNFROMDIR/local.conf conf
 
 cat <<EOF
 Your build environemnt has been configured with:
